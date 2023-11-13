5c7080171adc4b7d093c86a7425c21942cfda849
Aki Wu <wuminzhe@gmail.com>
2023-11-13 18:59:57 +0800

    update decode

--- lib/abi_coder_rb/decode.rb
+++ lib/abi_coder_rb/decode.rb
@@ -1,5 +1,3 @@
-require_relative "decode/decode_type"
-require_relative "decode/decode_types"
 require_relative "decode/decode_tuple"
 require_relative "decode/decode_fix_array"
 require_relative "decode/decode_array"
@@ -11,5 +9,49 @@ module AbiCoderRb
     types = types.map { |type| type.is_a?(Type) ? type : Type.parse(type) }
 
     decode_types(types, data)
+  end
+
+  private
+
+  def decode_type(type, data)
+    return nil if data.nil? || data.empty?
+
+    case type
+    when Tuple ## todo: support empty (unit) tuple - why? why not?
+      decode_tuple(type, data)
+    when FixedArray # static-sized arrays
+      decode_fix_array(type, data)
+    when Array
+      decode_array(type, data)
+    else
+      decode_primitive_type(type, data)
+    end
+  end
+
+  def decode_types(types, data)
+    start_positions = start_positions(types, data)
+
+    types.map.with_index do |type, index|
+      start_position = start_positions[index]
+      decode_type(type, data[start_position..])
+    end
+  end
+
+  def start_positions(types, data)
+    start_positions = ::Array.new(types.size)
+    offset = 0
+
+    types.each_with_index do |type, index|
+      if type.dynamic?
+        # 读取动态类型的偏移量
+        start_positions[index] = decode_uint256(data[offset, 32])
+        offset += 32
+      else
+        start_positions[index] = offset
+        offset += type.size
+      end
+    end
+
+    start_positions
   end
 end
--- lib/abi_coder_rb/decode/decode_primitive_type.rb
+++ lib/abi_coder_rb/decode/decode_primitive_type.rb
@@ -1,7 +1,4 @@
 module AbiCoderRb
-  # 和原来的代码最不同的地方，data不再是正好的，而是包含了 解码所需的数据 和 剩余的数据。
-  # 这样在入口处，也就是decoder.decode 方法中不再需要为每个类型都计算准确的 解码所需的数据。
-  # 从而简化了代码。
   def decode_primitive_type(type, data)
     case type
     when Uint
