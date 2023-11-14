eab5e028565e8cb87b98c73793fd6fd30baab5ae
Aki Wu <wuminzhe@gmail.com>
2023-11-14 19:26:09 +0800

    wasm modify

--- lib/abi_coder_rb.rb
+++ lib/abi_coder_rb.rb
@@ -32,12 +32,12 @@ module AbiCoderRb
 
   def hex_to_bin(hex) # convert hex(adecimal) string  to binary string
     hex = hex[2..] if %w[0x 0X].include?(hex[0, 2]) ## cut-of leading 0x or 0X if present
-    [hex].pack("H*")
+    hex.scan(/../).map { |x| x.hex.chr }.join
   end
   alias hex hex_to_bin
 
   def bin_to_hex(bin) # convert binary string to hex string
-    bin.unpack1("H*")
+    bin.each_byte.map { |byte| "%02x" % byte }.join
   end
   alias bin bin_to_hex
 end
