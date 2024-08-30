52c31b4de4e690c87c46956ef5fe8c4db147396a
Aki Wu <wuminzhe@gmail.com>
2024-08-30 23:42:08 +0800

    update

--- lib/abi_coder_rb/parser.rb
+++ lib/abi_coder_rb/parser.rb
@@ -8,26 +8,40 @@ module AbiCoderRb
       class << self
         def parse(abi_type_str)
           abi_type = AbiCoderRb::AbiParser.new(abi_type_str).parse
-          _create(abi_type)
+          create_type(abi_type)
         end
 
         private
-        def _create(abi_type)
+
+        def create_type(abi_type)
           case abi_type[:type]
           when 'string' then String.new
           when 'address' then Address.new
           when 'bool' then Bool.new
-          when 'uint' then Uint.new(abi_type[:bits])
-          when 'int' then Int.new(abi_type[:bits])
-          when 'bytes' then abi_type[:length] ? FixedBytes.new(abi_type[:length]) : Bytes.new
-          when 'tuple' then Tuple.new(abi_type[:inner_types].map { |t| _create(t) })
-          when 'array'
-            abi_type[:length] ?
-              FixedArray.new(_create(abi_type[:inner_type]), abi_type[:length]) :
-              Array.new(_create(abi_type[:inner_type]))
+          when 'uint', 'int' then create_numeric_type(abi_type)
+          when 'bytes' then create_bytes_type(abi_type)
+          when 'tuple' then create_tuple_type(abi_type)
+          when 'array' then create_array_type(abi_type)
           else
             raise ParseError, "Unknown type: #{abi_type}"
           end
+        end
+
+        def create_tuple_type(abi_type)
+          Tuple.new(abi_type[:inner_types].map { |t| create_type(t) })
+        end
+
+        def create_numeric_type(abi_type)
+          abi_type[:type] == 'uint' ? Uint.new(abi_type[:bits]) : Int.new(abi_type[:bits])
+        end
+
+        def create_bytes_type(abi_type)
+          abi_type[:length] ? FixedBytes.new(abi_type[:length]) : Bytes.new
+        end
+
+        def create_array_type(abi_type)
+          inner_type = create_type(abi_type[:inner_type])
+          abi_type[:length] ? FixedArray.new(inner_type, abi_type[:length]) : Array.new(inner_type)
         end
       end
     end
