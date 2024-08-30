202e7c37542ea0225696a89accaf0c7752e60f5a
Aki Wu <wuminzhe@gmail.com>
2024-08-30 23:37:23 +0800

    update

--- lib/abi_coder_rb/parser.rb
+++ lib/abi_coder_rb/parser.rb
@@ -5,26 +5,29 @@ module AbiCoderRb
     class ParseError < StandardError; end
 
     class Parser
-      def self.parse(abi)
-        top_type = AbiCoderRb::AbiParser.new(abi).parse
-        _create(top_type)
-      end
+      class << self
+        def parse(abi_type_str)
+          abi_type = AbiCoderRb::AbiParser.new(abi_type_str).parse
+          _create(abi_type)
+        end
 
-      def self._create(abi_type)
-        case abi_type[:type]
-        when 'string' then String.new
-        when 'address' then Address.new
-        when 'bool' then Bool.new
-        when 'uint' then Uint.new(abi_type[:bits])
-        when 'int' then Int.new(abi_type[:bits])
-        when 'bytes' then abi_type[:length] ? FixedBytes.new(abi_type[:length]) : Bytes.new
-        when 'tuple' then Tuple.new(abi_type[:inner_types].map { |t| _create(t) })
-        when 'array'
-          abi_type[:length] ?
-            FixedArray.new(_create(abi_type[:inner_type]), abi_type[:length]) :
-            Array.new(_create(abi_type[:inner_type]))
-        else
-          raise ParseError, "Unknown type: #{abi_type}"
+        private
+        def _create(abi_type)
+          case abi_type[:type]
+          when 'string' then String.new
+          when 'address' then Address.new
+          when 'bool' then Bool.new
+          when 'uint' then Uint.new(abi_type[:bits])
+          when 'int' then Int.new(abi_type[:bits])
+          when 'bytes' then abi_type[:length] ? FixedBytes.new(abi_type[:length]) : Bytes.new
+          when 'tuple' then Tuple.new(abi_type[:inner_types].map { |t| _create(t) })
+          when 'array'
+            abi_type[:length] ?
+              FixedArray.new(_create(abi_type[:inner_type]), abi_type[:length]) :
+              Array.new(_create(abi_type[:inner_type]))
+          else
+            raise ParseError, "Unknown type: #{abi_type}"
+          end
         end
       end
     end
