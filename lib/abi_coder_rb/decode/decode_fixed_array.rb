c5b541aa1882c9345d609aadb154fdd2a7b2010f
Aki Wu <wuminzhe@gmail.com>
2024-08-31 15:04:27 +0800

    clear code

--- lib/abi_coder_rb/decode/decode_fixed_array.rb
+++ lib/abi_coder_rb/decode/decode_fixed_array.rb
@@ -1,13 +1,12 @@
 module AbiCoderRb
   def decode_fixed_array(type, data)
-    l = type.length
     inner_type = type.inner_type
 
     if inner_type.dynamic?
-      start_positions = (0...l).map { |i| decode_uint256(data[32 * i, 32]) } << data.size
+      start_positions = (0...type.length).map { |i| decode_uint256(data[32 * i, 32]) } << data.size
       outputs = start_positions.each_cons(2).map { |start, stop| data[start...stop] }
     else
-      outputs = (0...l).map { |i| data[inner_type.size * i, inner_type.size] }
+      outputs = (0...type.length).map { |i| data[inner_type.size * i, inner_type.size] }
     end
 
     outputs.map { |out| decode_type(inner_type, out) }
